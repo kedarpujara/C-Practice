fabfe6a34d245f91060805c9b91a0ab8f9e8d062
Kedar <kedarpujara@gmail.com>
2017-05-15 03:29:03 -0700

    Created a singly linked list in C# to practice my C# skills

---
+++ Test1/EmptyClass.cs
@@ -0,0 +1,47 @@
+﻿////using System;
+
+//namespace Test1
+//{
+//    class Program
+//    {
+
+//        public double width;
+//        private double length;
+//        internal double height;
+//        protected double weight;
+
+
+//        static void Main(string[] args)
+//        {
+//            Console.WriteLine("Hello World!");
+//            Console.WriteLine(addNum(20,21));
+//            int[] ar = forLoop();
+//            Console.WriteLine(ar);
+//            forEach();
+//            //Console.Read();
+//        }
+
+//        public static int addNum(int a, int b) {
+//            return a + b;
+//        }
+
+//        public static int[] forLoop() {
+//            int[] sortArray = new int[10];
+//            for (int i = 0; i < sortArray.Length; i++) {
+//                sortArray[i] = i;
+//                //Console.WriteLine(sortArray[i]);
+//            }
+//            //Console.WriteLine(sortArray);
+//            return sortArray;
+//        }
+
+//        public static void forEach() {
+//            int[] sortArray = new int[5];
+//            foreach(int i in sortArray) {
+//                int j = i + 10;
+//                Console.WriteLine("j is: " + j);
+//            }
+
+//        }
+//    }
+//}
---
+++ Test1/LinkedList.cs
@@ -0,0 +1,150 @@
+﻿using System;
+namespace LinkedListC
+{
+	public class Node
+	{
+		public Node next;
+		public int data;
+
+		public Node()
+		{
+			next = null;
+			data = 0;
+		}
+
+		//Constructor for a Node
+		public Node(Node n, int d)
+		{
+			this.next = n;
+			this.data = d;
+		}
+
+		public void setData(int d)
+		{
+			this.data = d;
+		}
+
+		public int getData()
+		{
+			return this.data;
+		}
+
+		public void setNext(Node n)
+		{
+			this.next = n;
+		}
+
+		public Node getNext()
+		{
+			return this.next;
+		}
+	}
+
+
+    public class LinkedList
+    {
+        private Node head;
+
+        public void printAllNodes()
+        {
+            if (head == null)
+                return;
+
+            Node curr = head;
+            while (curr != null)
+            {
+                Console.Write(curr.data);
+                if (curr.getNext() != null)
+                    Console.Write("->");
+                curr = curr.next;
+            }
+        }
+
+        public void addFront(int d)
+        {
+            Node newNode = new Node();
+            //first node being added
+            if (head == null)
+            {
+                head = newNode;
+                head.data = d;
+            }
+            else
+            {
+                newNode.data = d;
+                newNode.next = head;
+                head = newNode;
+            }
+        }
+
+        public void addEnd(int d)
+        {
+            Node newNode = new Node();
+            if (head == null)
+            {
+                head = newNode;
+                head.data = d;
+            }
+            else
+            {
+                Node curr = head;
+                while (curr.getNext() != null) curr = curr.next;
+                curr.next = newNode;
+                newNode.data = d;
+            }
+        }
+
+        public void addMiddle(int d, int pos)
+        {
+            Node newNode = new Node();
+            if (head == null)
+            {
+                head = newNode;
+                head.data = d;
+            }
+            else
+            {
+                Node curr = head;
+                Node temp;
+                int i = 0;
+                while (i < pos - 1 && curr.getNext() != null)
+                {
+                    curr = curr.next;
+                    i++;
+                }
+                temp = curr.next;
+                curr.next = newNode;
+                newNode.next = temp;
+                newNode.data = d;
+            }
+       }
+
+        public Boolean isEmpty() {
+            return (head == null);
+        }
+    
+
+
+	}
+
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			LinkedList lList = new LinkedList();
+            //Node newNode1 = new Node()
+            Console.WriteLine(lList.isEmpty());
+			lList.addFront(5);
+			lList.addFront(4);
+			lList.addFront(3);
+            lList.addFront(1);
+            lList.addFront(0);
+            lList.addEnd(6);
+            lList.addMiddle(2,2);
+			lList.printAllNodes();
+            Console.WriteLine();
+            Console.WriteLine(lList.isEmpty());
+		}
+	}
+
+}
